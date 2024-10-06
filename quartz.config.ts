102a2b1f2200656bcdcf8be68b705454e8555221
Henry Boyd <henry@techandconsequence.com>
2024-10-05 23:47:33 -0600

    Quartz sync: Oct 5, 2024, 11:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "henrygboyd.github.io/tech-and-consequence",
+    baseUrl: "techandconsequence.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
